7f8fb8911858a16264ffd1462c94029fbe28f964
derektroy <L00170167@student.lyit.ie>
2021-09-27 21:44:01 +0100

    Second Commit

--- number_game/number_game.py
+++ number_game/number_game.py
@@ -11,15 +11,15 @@
 """
 import random
 # Input Lowest Number
-lowest = int(input("Enter Lowest Number : -"))
+lowest = int(input("Enter Lowest Number : "))
 
 # Input Upper or Largest Number
-upper = int(input("Enter Largest Number : -"))
+upper = int(input("Enter Largest Number : "))
 
 # Generate Random Number Between two Inputs
-rndmnmbr = random.randint(lower, upper)
+rx = random.randint(lowest, upper)
 print("\n\tYou've only ",
-       round(math.log(upper - lower + 1, 2)),
+       round(math.log(upper - lowest + 1, 2)),
       " chances to guess the integer!\n")
 
 #Count of Guesses
@@ -27,25 +27,25 @@ count = 0
 
 # for calculation of minimum number of
 # guesses depends upon range
-while count < math.log(upper - lower + 1, 2):
+while count < math.log(upper - lowest + 1, 2):
     count += 1
 
     # taking guessing number as input
     guess = int(input("Guess a number:- "))
 
     # Condition testing
-    if x == guess:
+    if rx == guess:
         print("Congratulations you did it in ",
               count, " try")
         # Once guessed, loop will break
         break
-    elif x > guess:
+    elif rx > guess:
         print("You guessed too small!")
-    elif x < guess:
+    elif rx < guess:
         print("You Guessed too high!")
 
 # If Guessing is more than required guesses,
 # shows this output.
-if count >= math.log(upper - lower + 1, 2):
-    print("\nThe number is %d" % x)
+if count >= math.log(upper - lowest + 1, 2):
+    print("\nThe number is %d" % rx)
     print("\tBetter Luck Next time!")
